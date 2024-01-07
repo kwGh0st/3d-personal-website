d20ccfc40e859635777cee5690a08b6d76bd1683
Karol Programuje <karol341654@gmail.com>
2024-01-07 20:45:52 +0100

    changes vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/kwGh0st/kwGh0st.git",
+  base: "/kwGh0st/",
   plugins: [react()],
 });
